fade58b19128b30d2d2da5978b813a77747c7cf4
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-24 01:02:58 +0600

    all blog are display in the dom

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,34 @@
+import { PiBookmarkSimple } from "react-icons/pi";
+const Blog = ({blog}) => {
+    const {cover_img,title,author,author_img,hashtags,reading_time,posted_date} =blog;
+    return (
+        <div className="space-y-4">
+            <img className="w-full h-[450px]" src={cover_img} alt="" />
+            <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                    <img className="w-[60px] h-[60px] rounded-4xl" src={author_img} alt="" />
+                    <div className="ml-2">
+                        <h4 className="text-[#111111] text-[24px] font-bold">{author}</h4>
+                        <p className="text-[rgba(17,17,17,.6)] text-base font-semibold ">{posted_date}</p>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center">
+                    <span className="text-[rgba(17,17,17,.6)] text-[20px] font-medium">{reading_time} min read</span>
+                    <button className="text-2xl ml-1 text-[rgba(17,17,17,.6)]"><PiBookmarkSimple /></button>
+                </div>
+            </div>
+            <h2 className="text-[#111111] text-[40px] font-bold">{title}</h2>
+
+            <p>
+                {
+                    hashtags.map(hash => <span className="ml-1 font-medium text-[rgba(17,17,17,.6)] text-xl"><a>{hash}</a></span>)
+                }
+            </p>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,10 +2,10 @@
 import profile from "../../assets/images/profile.png"
 const Header = () => {
     return (
-        <div className="flex justify-between items-center max-w-11/12 mx-auto my-[40px]">
+        <header className="flex justify-between items-center max-w-11/12 mx-auto my-[40px]">
             <h1 className='lg:text-[40px] text-3xl text-[#111111] font-bold '>Knowledge Cafe</h1>
             <img className="w-[60px] h-[60px]" src={profile} alt="" />
-        </div>
+        </header>
     );
 };
 
