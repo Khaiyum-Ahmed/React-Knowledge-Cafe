a82c2c257aab675d79be0131f4e8c16b53f1efd8
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-23 18:31:06 +0600

    header section added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 
-
+import profile from "../../assets/images/profile.png"
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-5xl bg-amber-300 p-3 text-center font-extrabold'>Knowledge Cafe</h1>
+        <div className="flex justify-between items-center max-w-11/12 mx-auto my-[40px]">
+            <h1 className='lg:text-[40px] text-3xl text-[#111111] font-bold '>Knowledge Cafe</h1>
+            <img className="w-[60px] h-[60px]" src={profile} alt="" />
         </div>
     );
 };
