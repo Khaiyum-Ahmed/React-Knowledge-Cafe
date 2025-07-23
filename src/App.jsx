ac230635a9f3a11aeb75c63757ff6a0d0f137614
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-23 17:43:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-amber-300 p-3 text-center font-extrabold'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
